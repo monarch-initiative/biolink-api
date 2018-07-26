e5e44d444739190e385452fbfb9d2060de2429c5
Deepak Unni <deepak.unni3@gmail.com>
2018-07-25 21:51:12 -0400

    Search for parent directories when looking for .git

--- biolink/__init__.py
+++ biolink/__init__.py
@@ -2,5 +2,5 @@ import git
 from ontobio.util.user_agent import get_user_agent
 
 NAME="biolink-api"
-VERSION=git.Repo().head.object.hexsha[:7]
+VERSION=git.Repo(search_parent_directories=True).head.object.hexsha[:7]
 USER_AGENT=get_user_agent(name=NAME, version=VERSION)
