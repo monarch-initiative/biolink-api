d5a6506702aa88b1dea6979880722c3e67aae5f9
Deepak Unni <deepak.unni3@gmail.com>
2018-11-04 15:20:48 +0530

    Add method to access configuration from config.yaml

--- biolink/settings.py
+++ biolink/settings.py
@@ -1,3 +1,5 @@
+import yaml
+
 # Flask settings
 FLASK_SERVER_NAME = 'localhost:8888'
 FLASK_DEBUG = True  # Do not use debug mode in production
@@ -11,3 +13,14 @@ RESTPLUS_ERROR_404_HELP = False
 # SQLAlchemy settings
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+CONFIG = 'conf/config.yaml'
+
+biolink_config = None
+
+def get_biolink_config():
+    global biolink_config
+    if biolink_config is None:
+        with open(CONFIG, 'r') as f:
+            biolink_config = yaml.load(f)
+    return biolink_config
