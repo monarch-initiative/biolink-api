abde195ed22f372af070201266de4a6e07301d45
Deepak Unni <deepak.unni3@gmail.com>
2018-11-26 17:38:20 -0800

    Improve /bioentityset/homologs/ to support multiple IDs and get associations based on a given relation

--- biolink/api/entityset/endpoints/geneset_homologs.py
+++ biolink/api/entityset/endpoints/geneset_homologs.py
@@ -2,8 +2,9 @@ import logging
 
 from flask import request
 from flask_restplus import Resource
-from biolink.datamodel.serializers import compact_association_set, association_results
+from biolink.datamodel.serializers import association_results
 from ontobio.golr.golr_associations import search_associations, GolrFields
+from ontobio.vocabulary.relations import HomologyTypes
 
 from biolink.api.restplus import api
 from biolink import USER_AGENT
@@ -12,26 +13,38 @@ MAX_ROWS=10000
 
 log = logging.getLogger(__name__)
 
+homology_types = [
+    HomologyTypes.Homolog.value,
+    HomologyTypes.Paralog.value,
+    HomologyTypes.Ortholog.value,
+    HomologyTypes.LeastDivergedOrtholog.value,
+    HomologyTypes.InParalog.value,
+    HomologyTypes.OutParalog.value,
+    HomologyTypes.Ohnolog.value,
+    HomologyTypes.Xenolog.value
+]
+
 parser = api.parser()
-parser.add_argument('subject', action='append', help='Entity ids to be examined, e.g. NCBIGene:9342, NCBIGene:7227, NCBIGene:8131, NCBIGene:157570, NCBIGene:51164, NCBIGene:6689, NCBIGene:6387')
+parser.add_argument('subject', required=True, action='append', help='Entity ids to be examined, e.g. NCBIGene:9342, NCBIGene:7227, NCBIGene:8131, NCBIGene:157570, NCBIGene:51164, NCBIGene:6689, NCBIGene:6387')
+parser.add_argument('relation', choices=homology_types, help='relation based on which to fetch associations. Default: {} ({})'.format(HomologyTypes.Homolog.value, HomologyTypes.Homolog.name), default=HomologyTypes.Homolog.value)
 
 class EntitySetHomologs(Resource):
 
     @api.expect(parser)
-    @api.marshal_list_with(compact_association_set)
+    @api.marshal_list_with(association_results)
     def get(self):
         """
         Returns homology associations for a given input set of genes
         """
         args = parser.parse_args()
+        subjects = args.get('subject')
+        del args['subject']
 
         M=GolrFields()
-        rel = 'RO:0002434'  # TODO; allow other types
         results = search_associations(
-            subjects=args.get('subject'),
+            subjects=subjects,
             select_fields=[M.SUBJECT, M.RELATION, M.OBJECT],
             use_compact_associations=True,
-            relation=rel,
             rows=MAX_ROWS,
             facet_fields=[],
             user_agent=USER_AGENT,
