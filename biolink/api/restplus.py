72cb03f1d4216f2e0066a21edf70cdd7085c9ca7
Kent Shefchek <kshefchek@gmail.com>
2021-07-16 19:59:43 -0400

    bump version

--- biolink/api/restplus.py
+++ biolink/api/restplus.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm.exc import NoResultFound
 
 log = logging.getLogger(__name__)
 
-api = Api(version='1.0.1', title='BioLink API',
+api = Api(version='1.1.0', title='BioLink API',
           license='BSD3',
           contact='info@monarchinitiative.org',
           description='API integration layer for linked biological objects.\n\n __Source:__ https://github.com/biolink/biolink-api/')
